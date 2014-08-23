73d5fe1a8ef483a16eb88b09ef08cb185eff6aa3
magzhan.karasayev <fallen.s4e@gmail.com>
2014-08-23 20:00:24 +0600

    master: Upd: take book page skeleton added

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,16 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
-  
+
+  def take = Action {
+    Ok(views.html.take())
+  }
+
+
 }
---
+++ app/models/Messages.scala
@@ -0,0 +1,14 @@
+package models
+
+/**
+ * @author magzhan.karasayev
+ * @since 8/23/14 7:41 PM
+ */
+object Messages {
+  def libraryTitle = "Электронная библятека"
+  def selectAction = "Выберите действие:"
+
+  object Actions {
+    def take = "Взять книгу"
+  }
+}
